51c43891f112745977efaf681cfc992591cb6b79
LucasTsar <95707242+LucasTsar@users.noreply.github.com>
2025-02-03 21:09:56 +0400

    Added failed login tests

--- Login.spec.ts
+++ Login.spec.ts
@@ -1,11 +1,40 @@
 import { test, expect } from '@playwright/test';
 
-test('SuccesfullLogin', async ({ page }) => {
+test('SuccessfulLogin', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
   await page.getByLabel('Username').click();
   await page.getByLabel('Username').fill('tomsmith');
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('SuperSecretPassword!');
   await page.getByRole('button', { name: ' Login' }).click();
+
+  // validate successful login and success message
   await expect(page.locator('#flash-messages')).toHaveText(/You logged into a secure area/);
+  await expect(page).toHaveURL(/secure/)
+});
+
+test('FailedLoginInvalidPassword', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('tomsmith');
+  await page.getByLabel('Username').press('Tab');
+  await page.getByLabel('Password').fill('WrongPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+
+  // validate error message and user remained on login page
+  await expect(page.locator('#flash-messages')).toHaveText(/Your password is invalid!/);
+  await expect(page).toHaveURL(/login/)
+});
+
+test('FailedLoginInvalidUsername', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('WrongUsername');
+  await page.getByLabel('Username').press('Tab');
+  await page.getByLabel('Password').fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+
+  //validate error message and user remained on login page
+  await expect(page.locator('#flash-messages')).toHaveText(/Your username is invalid!/);
+  await expect(page).toHaveURL(/login/)
 });
