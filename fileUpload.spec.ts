77537157772ab7481bf08fa28a4d42476098ef3c
LucasTsar <95707242+LucasTsar@users.noreply.github.com>
2025-02-03 23:50:15 +0400

    Added file upload test

---
+++ fileUpload.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import path from 'path';
+
+test('File Upload - Verify Successful Upload', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/upload');
+
+  // Define the path to the file to upload
+  const filePath = path.join(__dirname, '..', 'the_internet/test-data', 'test.txt');
+
+  // Click choose file and select file
+  await page.locator('#file-upload').click();
+  await page.locator('#file-upload').setInputFiles(filePath);
+  
+  // Click Upload the file
+  await page.getByRole('button', { name: 'Upload' }).click();
+
+  // Wait for the upload confirmation message to appear
+  await page.waitForSelector('#content', { state: 'visible' });
+
+  // Verify that the file was uploaded successfully
+  await expect(page.locator('#content')).toHaveText(/File Uploaded!/);
+
+  // Verify the uploaded file name
+  await expect(page.locator('#content')).toHaveText(/test.txt/);
+});
+
